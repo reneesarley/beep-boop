964b7275694d2b9643bc48cc28a1dbb795dac67d
Renee Sarley <reneesarley@gmail.com>
2018-06-08 11:05:09 -0700

    connect front end to back end with beepBoopConverter calling convertedMessage function

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,32 @@
+// user logic
+$(function() {
+  $("#numberForm").submit(function(event){
+    event.preventDefault();
+    var userInput = $("#numberInput").val();
+    var userNumber = parseInt(userInput);
+    console.log("Users input is: " + userNumber);
+    createUsersArray(userNumber);
+  });
+});
+
+function convertedMessage(updatedNumbers){
+  $("#results").text(updatedNumbers);
+}
+
 // business logic
 function createUsersArray(numberForArray){
+  console.log("this is the number that hits createUsersArray" + numberForArray);
   var usersArray = [];
   for (var i = 1; i <= numberForArray; i++) {
    usersArray.push(i);
   }
+  console.log("usersArray after for loop to create array: " + usersArray);
    beepBoopConverter(usersArray)
 }
 
 function beepBoopConverter(arrayToConvert){
   var convertedNumber
   var convertedArray = [];
-  // var testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
   for(var i = 0; i < arrayToConvert.length; i ++){
     if((arrayToConvert[i] % 3) === 0 && arrayToConvert[i] != 0){
       convertedNumber = "I'm sorry, Dave. I'm afraid I can't do that."
@@ -24,19 +40,6 @@ function beepBoopConverter(arrayToConvert){
     convertedArray.push(convertedNumber);
     console.log(arrayToConvert[i] + "=" + convertedNumber);
   }
-  console.log(convertedArray);
+  console.log("this is the converted array: " + convertedArray);
+  convertedMessage("this is a test of the back end callin the front end");
 }
-
-// user logic
-$(function() {
-  $("#numberForm").submit(function(event){
-    event.preventDefault();
-    var userNumber = $("#numberInput").val();
-    console.log("Users input is: " + userNumber);
-    beepBoopConverter(userNumber);
-  });
-
-  function returnConvertedNumbers(updatedNumbers){
-    $("#results").text(updatedNumbers);
-  }
-});
