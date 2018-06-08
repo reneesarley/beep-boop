326723f6113f36ec182eac0d76656ff9e696f980
Renee Sarley <reneesarley@gmail.com>
2018-06-08 09:48:00 -0700

    refactor to removed redundant conditionals for single and multiple digit numbers resulting in beep and boop

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,6 @@ function beepBoopConverter(numberToConvert){
   if((numberToConvert % 3) === 0 && numberToConvert != 0){
     convertedNumber = "I'm sorry, Dave. I'm afraid I can't do that."
     return convertedNumber
-  } else if(numberToConvert === 1){
-    convertedNumber = "Boop"
-    return convertedNumber
-  } else if(numberToConvert === 0){
-    convertedNumber = "Beep"
-    return convertedNumber
   } else if((numberToConvert.toString()).includes("1")){
     convertedNumber = "Boop"
     return convertedNumber
