6c7df28dc8204740e2f072d9771563fe0ee9d576
Renee Sarley <reneesarley@gmail.com>
2018-06-08 12:22:27 -0700

    add conditional to give class to Beep list items

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,15 @@ $(function() {
 });
 
 function convertedMessage(updatedNumbers){
-  $(".results").text(updatedNumbers);
+  // $(".results").text(updatedNumbers);
   $(".results").removeClass("hidden");
+  for(var i = 0; i < updatedNumbers.length; i++){
+    if(updatedNumbers[i] === "Beep!"){
+      $(".results ul").append("<li class='beep'>"+ updatedNumbers[i]+ "</li>");
+    } else{
+    $(".results ul").append("<li>"+ updatedNumbers[i]+ "</li>");
+    }
+  }
 }
 
 // business logic
@@ -28,7 +35,6 @@ function beepBoopConverter(arrayToConvert){
   for(var i = 0; i < arrayToConvert.length; i ++){
     if((arrayToConvert[i] % 3) === 0 && arrayToConvert[i] != 0){
       convertedNumber = "I'm sorry, Dave. I'm afraid I can't do that."
-      console.log("the number is " + arrayToConvert[i] + " I'm sorry, Dave")
     } else if((arrayToConvert[i].toString()).includes("1")){
       convertedNumber = "Boop!"
     } else if((arrayToConvert[i].toString()).includes("0")){
