652b6e446765ee837d7cb04e391c41f1c2d2e3c5
Renee Sarley <reneesarley@gmail.com>
2018-06-08 09:04:46 -0700

    add condition to turn 1 into boop

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,10 @@ function beepBoopConverter(numberToConvert){
   // convert number input to string to emulate user inputs
   var stringNumber = numberToConvert.toString()   // convert number input to string to emulate user inputs
   var convertedNumber
-
-  if(stringNumber.includes("0")){
+  if(stringNumber.includes("1")){
+    convertedNumber = "Boop"
+    return convertedNumber
+  } else if(stringNumber.includes("0")){
     convertedNumber = "Beep"
     return convertedNumber
   } else {
