6bbafeb1ca8f9ed3c4cdd02b44448c7c74b65d8a
Renee Sarley <reneesarley@gmail.com>
2018-06-08 11:52:18 -0700

    style h1 and form

--- js/scripts.js
+++ js/scripts.js
@@ -4,23 +4,21 @@ $(function() {
     event.preventDefault();
     var userInput = $("#numberInput").val();
     var userNumber = parseInt(userInput);
-    console.log("Users input is: " + userNumber);
     createUsersArray(userNumber);
   });
 });
 
 function convertedMessage(updatedNumbers){
-  $("#results").text(updatedNumbers);
+  $(".results").text(updatedNumbers);
+  $(".results").removeClass("hidden");
 }
 
 // business logic
 function createUsersArray(numberForArray){
-  console.log("this is the number that hits createUsersArray" + numberForArray);
   var usersArray = [];
-  for (var i = 1; i <= numberForArray; i++) {
+  for (var i = 0; i <= numberForArray; i++) {
    usersArray.push(i);
   }
-  console.log("usersArray after for loop to create array: " + usersArray);
    beepBoopConverter(usersArray)
 }
 
@@ -30,16 +28,15 @@ function beepBoopConverter(arrayToConvert){
   for(var i = 0; i < arrayToConvert.length; i ++){
     if((arrayToConvert[i] % 3) === 0 && arrayToConvert[i] != 0){
       convertedNumber = "I'm sorry, Dave. I'm afraid I can't do that."
+      console.log("the number is " + arrayToConvert[i] + " I'm sorry, Dave")
     } else if((arrayToConvert[i].toString()).includes("1")){
-      convertedNumber = "Boop"
+      convertedNumber = "Boop!"
     } else if((arrayToConvert[i].toString()).includes("0")){
-      convertedNumber = "Beep"
+      convertedNumber = "Beep!"
     } else {
       convertedNumber = arrayToConvert[i]
     }
     convertedArray.push(convertedNumber);
-    console.log(arrayToConvert[i] + "=" + convertedNumber);
   }
-  console.log("this is the converted array: " + convertedArray);
-  convertedMessage("this is a test of the back end callin the front end");
+  convertedMessage(convertedArray);
 }
